d1e2b2b5aa8e9ca61bd13506ddb082677f298668
DownaB <61759222+DownaB@users.noreply.github.com>
2020-06-18 17:29:34 +0200

    toolbar retour

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/PlugNeighbourActivity.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/PlugNeighbourActivity.java
@@ -3,8 +3,10 @@ package com.openclassrooms.entrevoisins.ui.neighbour_list;
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -29,9 +31,7 @@ private TextView mPhoneNumber;
 private TextView mInternetLink;
 private TextView mAboutMe;
 private Neighbour neighbour;
-private String avatar; 
-
-
+private String avatar;
 
 
 
@@ -39,6 +39,8 @@ private String avatar;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plug_neighbour);
+        setSupportActionBar(findViewById(R.id.toolbar2));
+        setupActionBar();
 
         mAvatar = findViewById(R.id.avatar);
         mFavorite = findViewById(R.id.favorite);
@@ -68,7 +70,17 @@ private String avatar;
 
     }
 
+    private void setupActionBar() {
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
-
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return (true);
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
